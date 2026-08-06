0931f4ae542ed57ceefc2b6a02c104245fb83847
Dewnan <dewnanchamithka2003@gmail.com>
2026-08-06 11:55:18 +0530

    fix: updated to make the app from android 8 +

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -16,10 +16,10 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.caredrop"
+        applicationId = "me.dewnan.caredrop"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 26
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
